a396eced88b7be192fb641332d03d9955775ba4b
jdsee <joscha-seelig@outlook.de>
2022-11-25 00:32:20 +0100

    Add basic polybar config

---
+++ home/polybar/default.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, user, ... }:
+
+{
+  services.polybar = {
+    enable = true;
+    script = "polybar bar &";
+    settings = {
+      "bar" = { };
+
+      "module/xkeyboard" = {
+        type = "internal/xkeyboard";
+        blacklist = [ "num lock" ];
+
+        label.layout = "%layout%";
+        # label.layout.foreground = "${colors.alert}";
+
+        label.indicator.padding = 2;
+        label.indicator.margin = 1;
+        # label.indicator.foreground = "${colors.background}";
+        # label.indicator.background = "${colors.secondary}";
+      };
+    };
+  };
+}
